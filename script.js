e97b012f74e44fc4804d72918337330a43618e36
ge0-rge <georgechkhaidze22@gmail.com>
2021-10-28 00:00:03 +0400

    done
    done

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 $('.slider').slick({
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 1500,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 1500,
 
-  });
-  
+});
+
+  let navigation = document.getElementById('navigation');
+  let burgericon = document.getElementById('burger');
+  burgericon.addEventListener('click',function(){
+    navigation.classList.toggle('active');
+  })
+
+document.getElementById('button2').addEventListener('click',function(){
+  alert('registered');
+})
